4a7bba3724b36be8dd6f9a8b8b1a27a3773148b2
prachib2011 <89467902+prachib2011@users.noreply.github.com>
2021-12-14 12:28:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function picture(){
+
+picture = document.getElementById("#picture").value;
+window.location="Piature Gallery";
+
+}
+
+function flavour(){
+
+    flavour = document.getElementById("#flavour").value;
+    window.location="Flavour of cake";
+    
+    }
+
+    function contact(){
+
+        contact = document.getElementById("#contact").value;
+        window.location="Contact";
+        
+        }
